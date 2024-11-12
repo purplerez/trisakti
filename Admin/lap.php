ddef74d8aeb7cd629c5482799256c2d25bc3177a
sabrinaauliya <160497085+sabrinaauliya@users.noreply.github.com>
2024-11-12 14:04:42 +0700

    Update lap.php

--- Admin/lap.php
+++ Admin/lap.php
@@ -5,6 +5,7 @@ require '../user/session_check.php';
 include "./adm/data.php";
 
 $transaksi = new TransaksiController;
+
 ?>
 
 <!DOCTYPE html>
@@ -65,12 +66,6 @@ $transaksi = new TransaksiController;
                 </div>
                 <ul class="sidebar-nav">
                     <li class="sidebar-item">
-                        <a href="edit.php?username=<?php echo $_SESSION['username']; ?>" class="sidebar-link">
-                            <i class="lni lni-user"></i>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
                         <a href="db_admin.php" class="sidebar-link">
                         <i class="lni lni-home"></i>
                             <span>Dashboard</span>
@@ -100,6 +95,12 @@ $transaksi = new TransaksiController;
                             <span>Data User</span>
                         </a>
                     </li>
+                    <li class="sidebar-item">
+                        <a href="edit.php?username=<?php echo $_SESSION['username']; ?>" class="sidebar-link">
+                            <i class="lni lni-user"></i>
+                            <span>Profile</span>
+                        </a>
+                    </li>
                 </ul>
                 <div class="sidebar-footer position-fixed bottom-0">
                     <a href="../logout.php" class="sidebar-link">
@@ -116,12 +117,12 @@ $transaksi = new TransaksiController;
                 <hr>
             </div>
             <div class="tanggal-container">
-                <form action="">
-                    <div class="tanggal">
-                        <input type="date" class="form-control">
-                        <button class="btn btn-primary">Cari</button>
-                    </div>
-                </form>
+            <form action="" method="GET">
+                <div class="tanggal">
+                    <input type="date" class="form-control" name="tanggal">
+                    <button class="btn btn-primary" type="submit">Cari</button>
+                </div>
+            </form>
             </div>
             <div class="table-lap mt-4">
                 <table class="custom-table">
@@ -133,34 +134,54 @@ $transaksi = new TransaksiController;
                             <th>Pelabuhan</th>
                             <th>Trip</th>
                             <th>Total Transaksi</th>
-                            <th colspan="3">Menu</th>
+                            <th colspan="4">Menu</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php 
-                            $data = $transaksi->view_transaksi(1);
-                            $no = 1;
-                            foreach($data as $rec) {
-                        ?> 
-                         <tr>
-                            <td><?= $no ?></td>
-                            <td><?= $rec['tanggal'] ?></td>
-                            <td><?= $rec['jam'] ?></td>
-                            <td><?= $rec['pelabuhan'] ?></td>
-                            <td><?= $rec['trip'] ?></td>
-                            <td>Rp <?= number_format($rec['total'], 0, ',', '.') ?></td>
+                    <?php 
+                    if (isset($_GET['tanggal'])) {
+                        $tanggal = $_GET['tanggal'];
+                        $data = $transaksi->view_transaksi_by_date($tanggal);
+                    } else {
+                        $data = $transaksi->view_transaksi();
+                    }
+
+                    $no = 1;
+                    if ($data && $data->num_rows > 0) {
+                        foreach ($data as $rec) {
+                            ?>
+                            <tr>
+                                <td><?= $no ?></td>
+                                <td><?= date('d-m-Y', strtotime($rec['tanggal'])) ?></td>
+                                <td><?= $rec['jam'] ?></td>
+                                <td><?= $rec['pelabuhan'] ?></td>
+                                <td><?= $rec['trip'] ?></td>
+                                <td style="text-align: left">Rp <?= number_format($rec['total'], 0, ',', '.') ?></td>
+                                <form action="transaksi.php?id=<?= $rec['id'] ?>" method="post">
+                                    <input type="hidden" name="id" value="<?= $rec['id'] ?>" />
+                                    <td><input type="submit" value="Edit" name="Edit" class="btn btn-primary"></td>
+                                </form>
                                 <form action="download.php" method="post">
                                     <input type="hidden" name="id" value="<?= $rec['id'] ?>" />
-                                <td><input type="submit" value="Excel" name="download" class="btn btn-primary"></td>
+                                    <td><input type="submit" value="Excel" name="download" class="btn btn-primary"></td>
                                 </form>
-
                                 <form action="pdf.php?id=<?= $rec['id'] ?>" method="post">
                                     <input type="hidden" name="id" value="<?= $rec['id'] ?>" />
-                                <td><input type="submit" value="Pdf" name="download" class="btn btn-primary"></td>
+                                    <td><input type="submit" value="Pdf" name="download" class="btn btn-primary"></td>
                                 </form>
+                                    <form action="lap.php" method="post" onsubmit="return confirm('Anda Yakin Ingin Menghapus Data?');">
+                                    <input type="hidden" name="id" value="<?= $rec['id'] ?>" />
+                                    <td><input type="submit" value="Delete" name="delLap" class="btn btn-primary"></td>
+                                </form>
+                            </tr>
+                            <?php   
+                            $no++;
+                        }
+                    } else {
+                        echo "<tr><td colspan='7' class='text-center'>Tidak ada data untuk tanggal yang dipilih.</td></tr>";
+                    }
+                    ?>
 
-                        </tr>
-                        <?php $no++; } ?>
                     </tbody>
                 </table>
             </div>
